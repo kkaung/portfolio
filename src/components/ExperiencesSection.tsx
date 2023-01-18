63e0bcf0e10bbbae760cae928d356b4fe0d4b151
moonGoblin <kaungzaw1996@gmail.com>
2023-01-18 12:29:15 +1100

    updated experence container

--- src/components/ExperiencesSection.tsx
+++ src/components/ExperiencesSection.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 
 export default function ExperiencesSection() {
@@ -9,7 +10,15 @@ export default function ExperiencesSection() {
             <div className="flex-1  mt-4 flex space-x-10 sm:ml-20 sm:mt-0">
                 <div className="flex justify-between w-full">
                     <div>
-                        <div>CB Bank</div>
+                        <div>
+                            CB Bank
+                            <Link
+                                target="_blank"
+                                href="https://www.cbbank.com.mm/en"
+                            >
+                                (www.cbbank.com.mm)
+                            </Link>
+                        </div>
                         <div className="text-sm text-gray-400">
                             Full Stack Developer
                         </div>
